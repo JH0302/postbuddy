6c41d261dc8168a4ddc36066c17e01a81cc6c654
Juhyeong <rkdwngud19987@gmail.com>
2020-04-07 15:10:24 -0400

    basic form

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3^8h7r5&(wol46$mg*=a@%14=cv$*7(qw$132pu3xbp7djt=ha'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = '*'
 
